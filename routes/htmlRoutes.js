43a41ea97eff2d8b4e7d14d182e2d45628484cbd
Evan Chu <echu34@gmail.com>
2021-01-30 16:19:19 -0500

    added route JS and gitignore

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,14 @@
+var path = require("path");
+
+
+module.exports = function(){
+
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "./public/assets/index.html"));
+    });
+
+    app.get("/notes", function(req, res) {
+        res.sendFile(path.join(__dirname, "./public/assets/notes.html"));
+    });
+
+};
