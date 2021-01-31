ef96bd8b1f4c371b52f234a0dfa6ce21135abb93
Evan Chu <echu34@gmail.com>
2021-01-31 11:37:41 -0500

    working on api Route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,1 +1,28 @@
-var db = require("./db/db.json")
+
+const fs = require("fs");
+const db = require("./db/db.json");
+
+module.exports = function(app){
+    
+    function writeToDB(notes){
+        notes = JSON.stringify(notes);
+        console.log(notes);
+
+        fs.writeFileSync("./db/db.json", notes, function(err){
+            if(err){
+                return console.log(err);
+            }
+        });
+    }
+    
+    app.post("/api/notes", function(req,res){
+        if()
+
+
+    })
+
+
+
+
+
+}
