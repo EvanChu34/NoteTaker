4d3234b6a4bf91353b5d776fcfb9075cf1d1b01c
Evan Chu <echu34@gmail.com>
2021-01-31 18:35:48 -0500

    adding gitignore and some edits

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,6 @@
 var path = require("path");
 
-
-module.exports = function(){
+module.exports = function(app){
 
     app.get("*", function(req, res) {
         res.sendFile(path.join(__dirname, "./public/assets/index.html"));
