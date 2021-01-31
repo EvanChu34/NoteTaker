2565f96064cf6678aafbb3937456c5ddf36d6a89
Evan Chu <echu34@gmail.com>
2021-01-31 14:38:52 -0500

    added more to API

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,6 @@
 
+const { json } = require("express");
+const e = require("express");
 const fs = require("fs");
 const db = require("./db/db.json");
 
@@ -16,13 +18,33 @@ module.exports = function(app){
     }
     
     app.post("/api/notes", function(req,res){
-        if()
+        if(notesData.length == 0 ){
+            req.body.id = "0"
+        } else{
+            req.body.id = JSON.stringify(JSON.parse(notesData[notesData.length - 1].id) +1)
+        }
+        console.log("req.body.id: " +req.body.id);
 
+        notesData.push(req.body);
 
-    })
+        writeToDB(notesData);
+        console.log(notesData);
 
+        res.json(res.body);
+    });
 
-
-
+    app.delete("/api/notes/:id", function(req,res){
+        let id = req.params.id.toString();
+        for (i=0; i< notesData.length; i++){
+            if(notesData[i].id == id){
+                console.log("match!");
+                res.send(notesData[i])
+                notesData.splice(i,1);
+                break;
+            }
+        }
+        
+        writeToDB(notesData);
+    });
 
 }
